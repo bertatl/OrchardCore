622982313602416615cb0eb55eb1663b7bf23711
NoReply Amazon <no-reply@amazon.com>
2024-11-25 21:30:23 +0000

    [Commit] Iteration 13 #errors = 10 <= 11.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/OrchardCore/OrchardCore.ContentManagement/DefaultContentManager.cs][CS0122][416,33] 'ContentElement.Data' is inaccessible due to its protection level``.

--- src/OrchardCore/OrchardCore.ContentManagement/DefaultContentManager.cs
+++ src/OrchardCore/OrchardCore.ContentManagement/DefaultContentManager.cs
@@ -413,7 +413,7 @@ namespace OrchardCore.ContentManagement
             buildingContentItem.ContentItemVersionId = _idGenerator.GenerateUniqueId(existingContentItem);
             buildingContentItem.DisplayText = existingContentItem.DisplayText;
             buildingContentItem.Latest = true;
-            buildingContentItem.Data = existingContentItem.Data?.DeepClone() as JObject ?? new JObject();
+            buildingContentItem.Content = existingContentItem.Content?.DeepClone() as JObject ?? new JObject();
 
             var context = new VersionContentContext(existingContentItem, buildingContentItem);
 

f8ca94b3bba2ed9491e7d37ff6e91e1651f48351
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:23:46 +0000

    Revert "[Commit] Iteration 20 #errors = 8 <= 8."
    This reverts commit 5691a9961c7ff6ac5bb049a27c4770adc1e90297.

--- src/OrchardCore/OrchardCore.ContentManagement/DefaultContentManager.cs
+++ src/OrchardCore/OrchardCore.ContentManagement/DefaultContentManager.cs
@@ -739,7 +739,7 @@ namespace OrchardCore.ContentManagement
 
             var context = new CloneContentContext(contentItem, cloneContentItem);
 
-            context.CloneContentItem.Apply(contentItem.Data?.DeepClone() as JObject);
+            context.CloneContentItem.Apply(contentItem.Data.DeepClone() as JObject);
             context.CloneContentItem.DisplayText = contentItem.DisplayText;
 
             await Handlers.InvokeAsync((handler, context) => handler.CloningAsync(context), context, _logger);
